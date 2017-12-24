fcd57c8b392b00d6ab9abdf89466c4a5ff421e29
Mohamed <malbgal@gmail.com>
2017-12-24 10:57:31 -0800

    Initial commit

---
+++ WebFrame.java
@@ -0,0 +1,236 @@
+package hw6;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.table.DefaultTableModel;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.CountDownLatch;
+/*
+The WebFrame should contain the GUI, keep pointers to the main elements,
+and manage the overall program flow.
+ */
+@SuppressWarnings("serial")
+public class WebFrame extends JFrame
+
+{
+
+	static ExecutorService threadPool;
+	static List<String> urls;
+	static List<Component> btns = new ArrayList<Component>();
+	//static int threadLimit = 30;     //for semaphore not initialized yet,correct value from instructions unclear
+	static boolean inRunState;
+	static ExecutorService singleThread;
+	WebFrame frame;
+	/*
+    can use threadPool.getActiveCount() to determine number of running threads.
+	 */
+
+	public WebFrame()
+	{
+		threadPool = Executors.newCachedThreadPool();
+		inRunState= false;
+		frame = this;
+
+	}
+
+	public JButton addStopButton()
+	{
+		JButton stop = new JButton("Stop");
+		stop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae)
+			{
+				killButtonVisibility("Stop");
+				if (singleThread != null)
+					singleThread.shutdownNow();
+				else if (threadPool != null)
+				{
+					try{threadPool.shutdownNow();}
+					catch(Exception e){
+						System.out.println("Threadpool couldn't shutdown:" + e.getMessage());
+					}
+				}
+			}
+
+		});
+		return stop;
+	}
+
+	public JButton addFetchButton(boolean concurrently)
+	{
+		JButton fetch;
+		if (concurrently)
+		{
+			fetch = new JButton("Fetch Concurrently");
+			fetch.addActionListener(new ActionListener()  {
+				public void actionPerformed(ActionEvent ae)
+				{
+					killButtonVisibility("Fetch Concurrently");
+					killButtonVisibility("Fetch");
+					launchWorkers(true);
+
+				}
+			});
+		}
+		else
+		{
+			fetch = new JButton("Fetch");
+			fetch.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent ae)
+				{
+					killButtonVisibility("Fetch Concurrently");
+					killButtonVisibility("Fetch");
+					launchWorkers(false);
+					;
+				}
+			});
+		}
+		return fetch;
+	}
+
+	public void killButtonVisibility(String buttonName)
+	{
+		for(Component elt: btns)
+		{
+			if (((AbstractButton) elt).getText().charAt(0) == buttonName.charAt(0))
+			{
+				//resetButtonVisibility();
+				elt.setVisible(false);
+				elt.invalidate();
+				frame.validate();
+				frame.repaint();
+			}
+		}
+	}
+
+	public void resetButtonVisibility()
+	{
+		for (Component elt: btns)
+		{
+			elt.setVisible(true);
+			elt.validate();
+			frame.validate();
+			frame.repaint();
+		}
+	}
+
+	public void launchWorkers(boolean concurrently)
+	{
+		Runnable launch;
+
+		if (!concurrently)
+		{
+			singleThread = Executors.newSingleThreadExecutor();
+			//ThreadPoolExecutor s = (ThreadPoolExecutor)WebFrame.singleThread;
+			launch = new Runnable() {
+				public void run()
+				{
+					for (int i = 0; i < urls.size(); i++)
+					{
+						singleThread.execute(new WebWorker(urls.get(i), i, frame));
+					}
+					threadPool.shutdown();
+					try {
+						threadPool.awaitTermination(10 * 1000, TimeUnit.NANOSECONDS);//(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+		}
+
+		else
+		{
+			launch = new Runnable() {
+				public void run()
+				{
+					for (int i = 0; i < urls.size(); i++)
+					{
+						threadPool.execute(new WebWorker(urls.get(i), i, frame));
+					}
+					threadPool.shutdown();
+					try{
+
+						threadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+
+					}catch(InterruptedException exc){
+						exc.printStackTrace();
+					}
+
+				}
+			};
+
+		}
+		new Thread(launch).start();
+	}
+
+	public static void main(String[] args)
+	{
+		WebFrame frame = new WebFrame();
+		frame.setLayout(new FlowLayout());
+		DefaultTableModel model = new DefaultTableModel(new String[] { "URL", "Status"}, 0);
+		JTable table = new JTable(model);
+		String url;
+		urls = new ArrayList<String>();
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+
+		String fileName = "";
+		if (args.length == 1)
+			fileName = args[0];
+		else
+			fileName = "http://www.sjsu.edu/cs/";
+		try (FileReader fr = new FileReader(fileName);
+				BufferedReader buffReader = new BufferedReader(fr);)
+		{
+			while ((url = buffReader.readLine()) != null)
+				urls.add(url);
+			//		urls.add("http://www.yahoo.com");
+			//		urls.add("http://www.reddit.com/r/wtf");
+			//		urls.add("http://www.espn.com");
+			for (String elt: urls)
+				model.addRow(new String[] { elt, "No Stats yet"});
+
+		}catch (FileNotFoundException exception){
+			exception.printStackTrace();
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+
+
+		JScrollPane scrollpane = new JScrollPane(table);
+		scrollpane.setPreferredSize(new Dimension(800,400));
+		frame.add(scrollpane);
+		JPanel p1 = new JPanel();
+		JButton f1 = frame.addFetchButton(true);
+		JButton f2 = frame.addFetchButton(false);
+		JButton s = frame.addStopButton();
+		p1.setLayout(new FlowLayout());
+		p1.add(f1);
+		btns.add(f1);
+		p1.add(f2);
+		btns.add(f2);
+		p1.add(s);
+		btns.add(s);
+		frame.add(p1);
+
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+
+	}
+}
---
+++ WebWorker.java
@@ -0,0 +1,113 @@
+package hw6;
+
+import java.io.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.swing.*;
+import java.util.Observable;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class WebWorker extends Thread
+{
+	/*
+  This is the core web/download i/o code...*/
+	String urlString;
+	int rowNumber;
+	WebFrame frame;
+	static Semaphore permit;
+	static List<String> urlResults;
+
+	public WebWorker(String url, int rowNumber, WebFrame frame)
+	{
+		this.rowNumber = rowNumber;
+		this.frame = frame;
+		urlString = url;
+		urlResults = new ArrayList<String>();
+		//permit = new Semaphore(WebFrame.threadLimit);	//not sure of number of semaphore limits
+	}
+
+	public void run() {
+		System.out.println("Fetching...." + urlString);
+		InputStream input = null;
+		StringBuilder contents = null;
+		try {
+			//permit.acquireUninterruptibly();
+			URL url = new URL(urlString);
+			URLConnection connection = url.openConnection();
+			// Set connect() to throw an IOException
+			// if connection does not succeed in this many msecs.
+			connection.setConnectTimeout(5000);
+			connection.connect();
+			input = connection.getInputStream();
+			BufferedReader reader  = new BufferedReader(new InputStreamReader(input));
+			char[] array = new char[1000];
+			int len;
+			contents = new StringBuilder(1000);
+			while ((len = reader.read(array, 0, array.length)) > 0) {
+				System.out.println("Fetching...." + urlString + len);
+				contents.append(array, 0, len);
+				Thread.sleep(10);
+			}
+			downLoad(contents.toString());
+				//permit.release();
+
+		}
+		// Otherwise control jumps to a catch...
+		catch(MalformedURLException ignored){
+			System.out.println("Exception: " + ignored.toString());
+		}
+		catch(InterruptedException exception){
+			// YOUR CODE HERE
+			// deal with interruption
+			/*
+				if interrupted, that means the 'stop' button was pressed.
+				use the reference to the frame to showFetchButtons and change state
+			 */
+
+			//if worker running, stop button is clickable, change so only fetch buttons are visible
+			if (frame != null)
+				{
+					frame.killButtonVisibility("Stop");
+					frame.resetButtonVisibility();
+				}
+			System.out.println("Button's visibility changed");
+
+		}
+		catch(IOException ignored) {
+			System.out.println("Exception: " + ignored.toString());
+		}
+		// "finally" clause, to close the input stream
+		// in any case
+		finally {
+			try{
+				if (input != null) input.close();
+			}catch(IOException ignored) {}
+		}
+
+
+	}
+
+	public void downLoad(String urlResult)
+	{
+		urlResults.add(urlResult);
+
+		System.out.println("task completed, in download\n");
+
+		/*
+		-WebWorker should have a download() method, called from its run(),
+		that tries to download the content of the url. The download() may or may not succeed for any number of reasons, it will probably take between a fraction of a second and a couple seconds to run
+
+		-> https://alvinalexander.com/java/edu/pj/pj010011
+		 */
+	}
+
+
+}
+
+
+
